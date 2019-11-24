2b216526236442f5af3f3eb06ba02d51adc16496
Bitha007 <bibekthapa922@gmail.com>
2019-11-24 19:30:10 +0545

    error fixed

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,8 +1,12 @@
-import "dotenv"
 import app from "firebase/app";
-import  "firebase/auth";
+import "firebase/auth";
 
-const  config = {
+const isProduction = process.env.NODE_ENV === "production";
+if (!isProduction) {
+   require("dotenv");
+}
+
+const config = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: "react-auth-eeded.firebaseapp.com",
   databaseURL: "https://react-auth-eeded.firebaseio.com",
@@ -12,8 +16,6 @@ const  config = {
   appId: "1:39576393321:web:0b3e744f8dcb40790974f8"
 };
 
-const Firebase = app.initializeApp(config)
-
-
+const Firebase = app.initializeApp(config);
 
 export default Firebase;
