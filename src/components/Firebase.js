27eb0d06793c572ff52b97b9a2617ebebe97b385
Bitha007 <bibekthapa922@gmail.com>
2019-11-24 18:58:48 +0545

    dotenv added

---
+++ src/components/Firebase.js
@@ -0,0 +1,19 @@
+import "dotenv"
+import app from "firebase/app";
+import  "firebase/auth";
+
+const  config = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: "react-auth-eeded.firebaseapp.com",
+  databaseURL: "https://react-auth-eeded.firebaseio.com",
+  projectId: "react-auth-eeded",
+  storageBucket: "",
+  messagingSenderId: "39576393321",
+  appId: "1:39576393321:web:0b3e744f8dcb40790974f8"
+};
+
+const Firebase = app.initializeApp(config)
+
+
+
+export default Firebase;
